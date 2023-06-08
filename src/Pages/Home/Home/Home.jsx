d39d3043a66d1c81f8fd042ca2b5f83152fbda16
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-06-08 22:36:54 +0600

    Navbar and Footer Section Add to the Project

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,6 @@
-import Navbar from "../../../Shared/Navbar/Navbar";
-
 const Home = () => {
   return (
     <div>
-      <Navbar></Navbar>
       <h2>This is Home</h2>
     </div>
   );
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-slate-100 bg-opacity-50">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -24,46 +27,36 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a>Item 1</a>
+              <Link>Dashboard</Link>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <Link>Home</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link>Instructors</Link>
+            </li>
+            <li>
+              <Link>Classes</Link>
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <Link>
+          <img className="w-44" src={logo} alt="web site logo" />
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
+            <Link>Dashboard</Link>
           </li>
           <li>
-            <a>Item 3</a>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link>Instructors</Link>
+          </li>
+          <li>
+            <Link>Classes</Link>
           </li>
         </ul>
       </div>
