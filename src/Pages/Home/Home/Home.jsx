ff7bc7ceee2a25f1bbf760287d5d9a1b9160c0c3
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-06-08 21:14:24 +0600

    Use Navbar to Home page. To Do need to Customize

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../../Shared/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2>This is Home</h2>
+    </div>
+  );
+};
+
+export default Home;
